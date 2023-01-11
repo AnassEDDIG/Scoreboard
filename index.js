5ce2832fb4078401a12840890cf942a4cf26aaa7
EDDIG Anass <EDDIG.ANASS@ofppt-edu.ma>
2023-01-11 12:44:37 +0100

    commit initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+let sol1=document.getElementById("H");
+let sol2=document.getElementById("G");
+let res1=0
+let res2=0
+function add1(){
+    res1+=1
+    sol1.textContent=(res1)
+}
+function add2(){
+    res1+=2
+    sol1.textContent=(res1)
+}
+function add3(){
+    res1+=3
+    sol1.textContent=(res1)
+}
+function add1G(){
+    res2+=1
+    sol2.textContent=(res2)
+}
+function add2G(){
+    res2+=2
+    sol2.textContent=(res2)
+}
+function add3G(){
+    res2+=3
+    sol2.textContent=(res2)
+}
